3f48bae9221dd540f59f28726ba17595f76c1317
Deepanshu Kushwaha <56316738+Deep2631@users.noreply.github.com>
2021-01-16 14:30:36 +0530

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,225 @@
+#this is my first program
+"""this new multi 
+docksring comment line
+
+print("hello world")
+if 5<2:
+    print("deepanshu")
+else:
+        print("kushwaha")"""
+"""
+x=5
+s = "deepanshu"
+e= "kushwaha"
+z= s+ " " +e
+print(z)
+
+
+"""
+"""
+x=5
+str= "deepanshu"
+print(type(x))
+print(type(str))
+y=2.5
+print(type(y))
+w= 1j
+print(type(w))
+"""
+"""
+
+x = 35e3
+print(type(x))
+y = 3+5j
+print(type(y))"""
+# casting interger 
+"""
+x = int (3.5)
+print(x)
+y=int (2.5)
+print(y)
+
+z= int ("3")
+print(z)
+"""
+#casting of float
+"""
+x= float(1)
+y= float(1.25)
+z= float("1.2155")
+print(x + y + z)
+print(x)
+
+print(y)
+
+
+print(z)
+"""
+# casting of string 
+"""
+x = str("deepanshu12365")
+y= str(1.2554)
+z= str(1)
+print(x)
+print(y)
+print(z)
+"""
+# print srting 
+"""
+x = 'deepnashu is a programmer '
+y = "deepanshu kushwaha is competitive programmer"
+z = '''Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+
+'''
+print(x)
+print(y)
+print(z)
+"""
+
+# string are array
+
+"""
+x = "deepanshu kushwaha i competitive programmer "
+if "deepanshu" not in x:
+     
+   print ("yes deepanshu is not  in x")
+"""
+
+# string as a array
+"""
+x="hellp world"
+
+print(x[3:7])
+"""
+# make lower case
+"""
+a= "DEEPANSHU KUSHWAA"
+print(a.lower())
+"""
+# make uppercase
+"""
+x= "deepanshu"
+print(x.upper())
+"""
+# replace string with another string
+"""
+x= "deepasnhu kushwaha"
+print(x.replace("p" , "h"))
+"""
+
+# split a string in to substring
+"""
+c= "deepanshu kushwaha"
+b= c.split(",")
+print(b)
+"""
+                        # OPERATOR IN PYHTON
+
+
+#addition operator 
+"""
+x= 5 
+v= 5 
+print(x+v)
+
+print(x-v)
+
+print(x*v)
+
+print(x/v)
+
+print(x%v)
+if x==v:
+    print("true")
+    
+"""
+#list in python
+"""
+mylist = ["deepanshu", "kumar", "kushwaha"]
+print(len(mylist))
+list1=[1,2,3,4,5,6,7,8,9]
+#access list
+print(list1[2])
+list2=[True,False,True]
+print(type(mylist)
+"""
+    
+# list as a constructor
+"""
+mylist=(("apple" , "banana", "orange", "deepan"))
+print(mylist)
+
+"""
+
+#loop through list
+"""
+mylist = ["apple", "banana", "cherry"]
+for x in mylist:
+  print(x)
+# if in list
+if "apple" not in mylist:
+    print("no")
+
+"""
+# add item in last inex in list(append)
+"""
+mylist=["deepanshu" ,"kumar"]
+print(mylist)
+mylist.append("kushwaha")
+print(mylist)
+
+# add element in specific index(insert(indexno, "element"))
+mylist.insert(1, "KUMAR")
+print(mylist)
+mylist.insert(3, 4521782)
+print(mylist)
+# remove element in list\
+
+mylist.remove("KUMAR")
+print(mylist)
+
+#remove from last element
+
+mylist.pop()
+print(mylist)
+# delete element from specific index
+
+
+del mylist[0]
+print(mylist)
+# make empty list
+
+mylist.clear()
+print(mylist)
+
+mylist.append("deepamshu" )
+print(mylist)
+mylist.insert(0, "mr")
+print(mylist)
+mylist.append("kushwaha")
+print(mylist)
+mylist.insert(2, "kumar")
+print(mylist)
+
+
+
+# Range of index
+
+print(mylist[1:])
+
+
+# navigating list reverse access
+print(mylist)
+
+print(mylist[-3])
+"""
+
+
+
+
+
+
+
+
+
+
